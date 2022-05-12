0777ffa32df6f1f1cf08a67a0d9eca8e45f29acf
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-12 18:11:10 +0430

    Updates

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        theme: 'light',
-    });
+    res.render('index');
 });
 
-app.listen(3000, 'localhost');
+app.listen(3000);
