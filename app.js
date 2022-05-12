256aa284ce92b34490231e264eff4c6fac0375f7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-12 17:28:43 +0430

    Added sending theme

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        theme: 'light',
+    });
 });
 
 app.listen(3000, 'localhost');
