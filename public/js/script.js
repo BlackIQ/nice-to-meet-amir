891bb79ac63866d1cec2485ae730f9e614c4d29a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-05-12 18:28:34 +0430

    Added change theme

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(() => {
     const ls = window.localStorage;
 
     if (ls.getItem('theme') === 'light') {
@@ -10,4 +10,28 @@ $(document).ready(function () {
         $('.navbar').addClass('navbar-dark');
         $('.btn-submit').addClass('btn-light');
     }
+
+    $('.adjust').click(() => {
+        if (ls.getItem('theme') === 'light') {
+            ls.setItem('theme', 'dark');
+
+            $('.main').removeClass('light');
+            $('.navbar').removeClass('navbar-light');
+            $('.btn-submit').removeClass('btn-dark');
+
+            $('.main').addClass('dark');
+            $('.navbar').addClass('navbar-dark');
+            $('.btn-submit').addClass('btn-light');
+        } else {
+            ls.setItem('theme', 'light');
+
+            $('.main').removeClass('dark');
+            $('.navbar').removeClass('navbar-dark');
+            $('.btn-submit').removeClass('btn-light');
+
+            $('.main').addClass('light');
+            $('.navbar').addClass('navbar-light');
+            $('.btn-submit').addClass('btn-dark');
+        }
+    });
 });
